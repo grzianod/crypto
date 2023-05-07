ecb5c39e41c5c3e5b3be9c14c615d76efe7913c6
grzianod <grazianodinocca@icloud.com>
2023-05-08 01:50:33 +0200

    v1.1.3

---
+++ lessons/python/challenges/fool_the_oracle_v2.py
@@ -0,0 +1,42 @@
+import os
+
+os.environ['PWNLIB_NOTERM'] = 'True'  # Configuration patch to allow pwntools to be run inside of an IDE
+os.environ['PWNLIB_SILENT'] = 'True'  # Configuration patch to tell pwntools to not send terminal strings to be printed
+
+from pwn import *
+from math import ceil
+from Crypto.Cipher import AES
+
+HOST = "130.192.5.212"
+PORT = 6542
+block_size = 16
+
+if __name__ == '__main__':
+
+    print(AES.block_size)
+    secret = b""
+    server = remote(HOST, PORT)
+    server.recvline()
+    server.recvline()
+    server.recvline()
+    server.recvline()
+
+    for n_blocks in reversed(range(4)):
+        prefix = b"0" * n_blocks * block_size
+        for i in range(block_size):
+            guess = b"0" * (block_size - (i+1))
+            for c in string.printable:
+                server.sendafter(b"> ", b"enc\n")
+                tampered = bytes(prefix + guess + secret + c.encode() + guess)
+                print("(" + str(n_blocks) + ", " + str(i) + ", " + c + "): " + str(tampered))
+                server.sendafter(b"> ", tampered.hex().encode() + b"\n")
+                output = server.recvline().decode()
+                tamp = output[4*2*block_size:5*2*block_size]
+                real = output[(5+3-n_blocks)*2*block_size:(6+3-n_blocks)*2*block_size]
+
+                if real == tamp:
+                    secret += c.encode()
+                    break
+
+    print("\n\nflag: " + secret.decode())
+    server.close()
